4510fb97be2d53ca5468b2ddac4fe0f0646bcc76
Hezekiah Gitenyi <hezekiahgitenyi24@gmail.com>
2026-08-14 12:28:49 -0500

    Added biome

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
+
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://hezekiahgitenyi.com',
-  vite: {
-    plugins: [tailwindcss()],
-  },
+	site: "https://hezekiahgitenyi.com",
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
